93bd6898459a8f30c518fdbd1857afd129b0cd1c
huzhang <huzhang@myweimai.com>
2021-11-22 22:23:22 +0800

    feat: 合并升序列表小顶堆实现

--- src/week4/10_mergeKLists.js
+++ src/week4/10_mergeKLists.js
@@ -48,3 +48,79 @@ var mergeTwoList = function (l, r) {
     dump.next = left ? left : right;
     return result.next;
 }
+
+
+
+/** --------------------------------------- */
+// 小顶堆实现
+
+var Node = function (val, list) {
+    this.val = val;
+    this.list = list;
+}
+
+var mergeKLists = function (lists) {
+    const n = lists.length;
+    if (n === 0) return null;
+    if (n === 1) return lists[0];
+    const heap = [null];
+    let dump = new ListNode(-1);
+    const result = dump;
+
+    for (let i = 0; i < n; i++) {
+        const val = lists[i] && lists[i].val;
+        if (val || val === 0) {
+            heap.push(new Node(val, lists[i].next))
+            heapifyUp(heap)
+        }
+    }
+
+    while (heap.length > 1) {
+        // 先堆化
+        const count = heap.length - 1;
+        for (let i = 1; i <= count >> 1; i++) {
+            heapifyDown(heap, i, count);
+        }
+        // 第一位取出来
+        dump.next = new ListNode(heap[1].val);
+        dump = dump.next;
+        const liveList = heap[1].list;
+        if (liveList) {
+            const node = new Node(liveList.val, liveList.next)
+            heap[1] = node;
+        } else {
+            heap.shift();
+            heap[0] = null;
+        }
+    }
+    return result.next
+}
+
+var heapifyUp = function (heap) {
+    let i = heap.length - 1;
+    while ((i >> 1) > 0 && heap[i].val < heap[i >> 1].val) {
+        swap(heap, i, i >> 1);
+        i = i >> 1;
+    }
+}
+
+var heapifyDown = function (heap, i, count) {
+    while (true) {
+        let min = i;
+        if (i * 2 <= count && heap[i * 2].val < heap[i].val) {
+            min = i * 2
+        }
+        if (i * 2 + 1 <= count && heap[i * 2 + 1].val < heap[min].val) {
+            min = i * 2 + 1
+        }
+        if (min === i) break;
+        swap(heap, min, i);
+        i = min;
+    }
+}
+
+var swap = function (array, a, b) {
+    [array[a], array[b]] = [array[b], array[a]]
+}
+
+
