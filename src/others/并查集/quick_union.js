d13c84d07dd39fb4b958da0ffd832e713de6d947
huzhang <huzhang@myweimai.com>
2022-01-04 14:21:43 +0800

    feat: save

--- src/others/并查集/quick_union.js
+++ src/others/并查集/quick_union.js
@@ -1,27 +1,35 @@
 var UnionSet = function (n) {
-    this.fathers = new Array(n+1);
-    this.size = new Array(n+1)
-    for (let i = 1; i <= n; i++) {
-        this.fathers[i] = i
-        this.size[i] = 1
-    }
-    
+	this.fathers = new Array(n + 1)
+	this.size = new Array(n + 1)
+	this.root = n
+	for (let i = 1; i <= n; i++) {
+		this.fathers[i] = i
+		this.size[i] = 1
+	}
 }
 
-UnionSet.prototype.find = function(x) {
-    if (this.fathers[x] === x) return x;
-    return this.find(this.fathers[x])
+UnionSet.prototype.find = function (x) {
+	while (this.fathers[x] !== x) {
+        this.fathers[x] = this.fathers[this.fathers[x]]
+		x = this.fathers[x]
+	}
+	return x
 }
 
-UnionSet.prototype.merge = function(a, b) {
-    const fa = this.find(a), fb = this.find(b);
-    if (fa === fb) return;
-    if (this.size[fa] < this.size[fb]) {
-        this.fathers[fa] = fb
-        this.size[fb] += this.size[fa]
-    } else {
-        this.fathers[fb] = fa
-        this.size[fa]+= this.size[fb]
-    }
-    
+UnionSet.prototype.merge = function (a, b) {
+	const fa = this.find(a),
+		fb = this.find(b)
+	if (fa === fb) return
+	if (this.size[fa] < this.size[fb]) {
+		this.fathers[fa] = fb
+		this.size[fb] += this.size[fa]
+	} else {
+		this.fathers[fb] = fa
+		this.size[fa] += this.size[fb]
+	}
+	this.root--;
+}
+
+UnionSet.prototype.getRoot = function() {
+    return this.root;
 }
