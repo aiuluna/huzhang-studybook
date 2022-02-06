ddfd9f9596acb93b55d0a645a4d48c3bc0970ee4
huzhang <huzhang@myweimai.com>
2022-02-06 22:22:17 +0800

    feat: save

---
+++ src/interview/webpack/src/index.js
@@ -0,0 +1,5 @@
+import {add} from './a';
+import hello from './hello.txt'
+
+console.log(add(3,4));
+console.log(hello)
---
+++ src/interview/webpack/webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	entry: './src/index.js',
+    module: {
+        rules: [{test: /\.txt$/, use: 'raw-loader'}]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    mode: 'development'
+}
