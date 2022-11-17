ba3c425c2fe15c066276b15765d45edb3a01b172
aiuluna <604401072@qq.com>
2022-11-18 00:18:38 +0800

    feat: add webpack study lessions

--- daydayup/table进度条/main.js
+++ daydayup/table进度条/main.js
@@ -1,7 +1,6 @@
 const mytable = document.querySelector('#mytable')
 const ro = new ResizeObserver((entries, observer) => {
   const table = entries[0].target;
-  console.log(table)
   table.querySelectorAll('tr').forEach((row, idx) => {
     row.style.setProperty('--row-height', `${row.offsetHeight}px`)
   })
